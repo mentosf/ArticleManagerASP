a3cec8c0b3b0be80ab8db126ac8dbe4f1dd7d7d6
mentos <olgrv36@gmail.com>
2026-05-20 20:53:22 +0300

    Models done

---
+++ Models/Article.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+namespace FinalTask.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(200, ErrorMessage = "Title can not be over 200 symbols")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Array content can not be empty")]
+        public string Content { get; set; } = string.Empty;
+
+        
+        [StringLength(500)]
+        public string Summary { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        
+        [Required]
+        public string AuthorId { get; set; } = string.Empty;
+
+        
+        public string AuthorName { get; set; } = string.Empty;
+
+        public bool IsPublished { get; set; } = false;
+
+        
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalTask.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Comment text can not be empty")]
+        [StringLength(1000)]
+        public string Text { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        
+        public int ArticleId { get; set; }
+        public Article? Article { get; set; }
+
+        // Who wrote comment (Keycloak UserId)
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        public string Username { get; set; } = string.Empty;
+    }
+}
