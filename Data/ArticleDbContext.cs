f9c8633c98650ff9447eb1d95e28c65cfe198f23
mentos <olgrv36@gmail.com>
2026-05-29 20:51:17 +0300

    DB connected (migrations done) tables created

--- Data/ArticleDbContext.cs
+++ Data/ArticleDbContext.cs
@@ -4,12 +4,12 @@ namespace FinalTask.Data
 {
     public class ArticleDbContext : DbContext
     {
+        public ArticleDbContext(DbContextOptions<ArticleDbContext> options) : base(options)
+        {
+        }
         public DbSet<Article> Articles { get; set; } = null!;
         public DbSet<Category> Categorys { get; set; } = null!;
         public DbSet<Comment> Comments { get; set; } = null!;
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Host=localhost;Database=ASP_FinalTask;Username=it_step_user;Password=bot15");
-        }
+        
     }
 }
